a1280eef16c7d8fc719681c6a010c6e36a23287a
Carlos Flores Martinez <cfloresm@anzen.com.mx>
2014-02-21 09:45:36 -0600

    More changes for use swager-ui

--- server.js
+++ server.js
@@ -1,45 +1,78 @@
-var express = require('express');
-var app = express();
+var express = require('express')
+, url = require("url")
+, swagger = require("swagger-node-express");
+ // , url = require("url")
+ // , swagger = require("swagger-node-express");
 
-app.use(express.methodOverride());
- 
+var app = express();
+app.use(express.json());
+app.use(express.urlencoded())
+
+swagger.setAppHandler(app);
 // ## CORS middleware
 //
 // see: http://stackoverflow.com/questions/7067966/how-to-allow-cors-in-express-nodejs
 var allowCrossDomain = function(req, res, next) {
-res.header('Access-Control-Allow-Origin', '*');
-res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
-res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization,X-BANK-TOKEN');
-// intercept OPTIONS method
-if ('OPTIONS' == req.method) {
-res.send(200);
-}
-else {
-next();
-}
+	res.header('Access-Control-Allow-Origin', '*');
+	res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+	res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization,X-BANK-TOKEN');
+	// intercept OPTIONS method
+	if ('OPTIONS' == req.method) {
+		res.send(200);
+	}
+	else {
+		next();
+	}
 };
+
 app.use(allowCrossDomain);
 
 
-
-app.get('/hello.txt', function(req, res){
-var body = 'Hello World';
-res.setHeader('Content-Type', 'text/plain');
-res.setHeader('Content-Length', Buffer.byteLength(body));
-res.end(body);
-console.log('execute GET method hello.txt');
+app.get('api/ping', function(req, res){
+	var body = 'pong';
+	res.setHeader('Content-Type', 'text/plain');
+	res.setHeader('Content-Length', Buffer.byteLength(body));
+	res.end(body);
+	console.log('execute GET method ping');
 });
 
-app.post('/login', function(req, res){
-console.log('execute POST method login');
+app.post('/api/login', function(req, res){
+	console.log('execute POST method login');
 
-  for(var item in req.headers) {
-    console.log(item + ": " + req.headers[item]);
-  }
-res.send(200);
+	for(var item in req.headers) {
+		console.log(item + ": " + req.headers[item]);
+	}
+	res.send(200);
+});
+
+app.get('/api/accounts/:type', function(req, res){
+	var typeAccount = req.params.type ;
+
+	if(typeAccount > 0 && typeAccount < 4){
+		console.log('execute GET method accounts ' + typeAccount);
+		console.log(typeAccount);
+		fs = require('fs')
+		fs.readFile('./jsonResponse/accounts'+ typeAccount +'.json', 'utf8', function (err,data) {
+			if (err) {
+				return console.log(err);
+			}
+			res.end(data);
+		});
+	}else{
+		res.end(404);
+	}
 });
 
 
+// function readFile(filename){
+// 	fs = require('fs')
+// 	fs.readFile('./jsonResponse/accounts'+ typeAccount +'.json', 'utf8', function (err,data) {
+// 		if (err) {
+// 			return console.log(err);
+// 		}
+// 		res.end(data);
+// 	});
+// }
 
 app.listen(3000);
 console.log('Listening on port 3000');
